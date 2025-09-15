f95492dabb7ab8e4cbfb7ef58e3ab729d4464166
MustafaAamir <mustafa@gmail.com>
2025-09-15 16:48:01 +0400

    clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,13 @@ export default function RootLayout({
 			gtag('config', '${GA_ID}');
 			`}
         </Script>
+        <Script type="text/javascript" strategy="afterInteractive">
+		  {`(function(c,l,a,r,i,t,y){
+			c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+			t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+			y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+			})(window, document, "clarity", "script", "tb581aw4qf");`}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
